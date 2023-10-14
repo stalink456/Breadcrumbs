0fb9809e9bd2f67c922c8da087b179913aa9af16
stalink456 <stas.litvin2010@yandex.ru>
2023-10-14 17:17:56 +0300

    added breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,20 +1,44 @@
 import React from "react";
-import { useLocation, Link, matchPath, RouteProps } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 interface IbreadcrumbNameMap {
   [key: string]: string;
 }
 
 const breadcrumbNameMap: IbreadcrumbNameMap = {
+  current: "Текущая страница",
   "/": "Главная страница",
-  "/secondPage/:second": "Вторая страница",
-  "/secondPage/:userId/thirdPage/:groupId": "Третья страница",
+  secondPage: "Вторая страница",
+  thirdPage: "Третья страница",
 };
 
 export const Breadcrumbs: React.FC = () => {
+  const location = useLocation()
+    .pathname.split("/")
+    .filter((element) => element);
+
   return (
     <div>
       <Link to="/">Главная страница</Link>
+      {location.length === 0 ? (
+        <>
+          <span>/</span>
+          <span>{breadcrumbNameMap.current}</span>
+        </>
+      ) : (
+        location.map((value, index) => (
+          <React.Fragment key={index}>
+            {index % 2 === 0 ? <span>/</span> : null}
+            {index + 2 === location.length ? (
+              <span>{breadcrumbNameMap[value]}</span>
+            ) : (
+              <Link to={`/${value}/${location[index + 1]}`}>
+                {breadcrumbNameMap[value]}
+              </Link>
+            )}
+          </React.Fragment>
+        ))
+      )}
     </div>
   );
 };
